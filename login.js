7ad91c234e82d7e57e6421030d565b312f292de3
popli610 <95101617+popli610@users.noreply.github.com>
2021-12-01 00:49:46 +0530

    Login page added

---
+++ login.js
@@ -0,0 +1,8 @@
+let username = document.querySelector('#username');
+let pass = document.querySelector('#password');
+
+function authenticate(form){
+    if(username.value=="nishant" && pass.value=="popli"){
+        location.href = "welcomePage.html";
+    }
+}
