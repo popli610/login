772eb4cc4f28eb102d1b7f6615577c09878538a3
popli610 <95101617+popli610@users.noreply.github.com>
2021-12-06 23:38:49 +0530

    updated the name

--- login.js
+++ login.js
@@ -3,6 +3,6 @@ let pass = document.querySelector('#password');
 
 function authenticate(form){
     if(username.value=="nishant" && pass.value=="popli"){
-        location.href = "welcomePage.html";
+        location.href = "welcomepage.html";
     }
 }
